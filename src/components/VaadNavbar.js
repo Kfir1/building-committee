5d191d04c5c08c06850cf116648ba5a13db67dd9
kfir <kfirrp@gmail.com>
2021-07-04 15:23:48 +0300

    link update

--- src/components/VaadNavbar.js
+++ src/components/VaadNavbar.js
@@ -10,23 +10,23 @@ class VaadNavbar extends React.Component{
 
 
 render(){
-    const login = ( ! this.props.activeUser) ? <Nav.Link href="/#/login"><strong>Login</strong></Nav.Link> : null
-    const signup = ( ! this.props.activeUser  ) ? <Nav.Link href="/#/signup"><strong>Signup</strong></Nav.Link> : null
-    const Dashboard = (  this.props.activeUser) ? <Nav.Link href="/#/dashboard"><strong>Dashboard</strong></Nav.Link> : null
-    const tenantsAccounts = ( this.props.activeUser && this.props.activeUser.isCommitteeMember  ) ?  <Nav.Link href="/#/tenantAccount"><strong>Tenant Account</strong></Nav.Link> : null
-    const voting = ( this.props.activeUser  ) ?  <Nav.Link href="/#/voting"><strong>voting</strong></Nav.Link> : null
+    const login = ( ! this.props.activeUser) ? <Nav.Link href="#/login"><strong>Login</strong></Nav.Link> : null
+    const signup = ( ! this.props.activeUser  ) ? <Nav.Link href="#/signup"><strong>Signup</strong></Nav.Link> : null
+    const Dashboard = (  this.props.activeUser) ? <Nav.Link href="#/dashboard"><strong>Dashboard</strong></Nav.Link> : null
+    const tenantsAccounts = ( this.props.activeUser && this.props.activeUser.isCommitteeMember  ) ?  <Nav.Link href="#/tenantAccount"><strong>Tenant Account</strong></Nav.Link> : null
+    const voting = ( this.props.activeUser  ) ?  <Nav.Link href="#/voting"><strong>voting</strong></Nav.Link> : null
 
     const userNameToShow = (  this.props.activeUser ) ? <Nav.Link> Hello {this.props.activeUser.name}</Nav.Link> : null
-    const logout = (this.props.activeUser) ? <Nav.Link href="/#/login/" onClick={ () => this.props.logout()}><strong>Log out</strong></Nav.Link> : null
+    const logout = (this.props.activeUser) ? <Nav.Link href="#/login/" onClick={ () => this.props.logout()}><strong>Log out</strong></Nav.Link> : null
   
     return(
         <Container >
         <Navbar bg="light" expand="lg" style={{background: "linear-gradient(180deg, rgba(42,139,219,1) 28%, rgba(232,232,232,1) 93%)"}}>
-            <Navbar.Brand href="/#/" style={{width: "77px"}}><Image style={{width: "77px", borderRadius:"50%"}} src="https://i.pinimg.com/736x/93/1d/6b/931d6b0f3e13feb6c8e3825f8e0ec3d8.jpg" thumbnail /></Navbar.Brand>
+            <Navbar.Brand href="#/" style={{width: "77px"}}><Image style={{width: "77px", borderRadius:"50%"}} src="https://i.pinimg.com/736x/93/1d/6b/931d6b0f3e13feb6c8e3825f8e0ec3d8.jpg" thumbnail /></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-                <Nav.Link href="/#/"><strong>Home</strong></Nav.Link>
+                <Nav.Link href="#/"><strong>Home</strong></Nav.Link>
                 {Dashboard}
                 {tenantsAccounts}
                 {voting}
